e71cd052df2449f75e8acb4c5749444bcc703193
sobolevn <mail@sobolevn.me>
2018-09-26 22:11:03 +0300

    Imporves tests and docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,11 +3,19 @@
 We follow Semantic Versions since the `0.1.0` release.
 
 
+## Version 0.1.2
+
+### Misc
+
+- Improves readme
+- Improves tests
+
+
 ## Version 0.1.1
 
 ### Bugfixes
 
-- Relies on `attrs` explicitly, see #2
+- Relies on `attrs` explicitly, see [#2](https://github.com/sobolevn/flake8-eradicate/issues/2)
 
 ### Misc
 
--- tests/fixtures/correct.py
+++ tests/fixtures/correct.py
@@ -1,5 +1,36 @@
 # -*- coding: utf-8 -*-
 
+"""
+Some docs string in a module.
+
+Some code example here:
+
+>>> x = (1 / 2) * 2
+>>> min(x, 0)
+0
+
+"""
+
 class Some(object):
+    """
+    Docstring inside a class.
+
+    Contains some code:
+
+        instance = Some()
+        print('Test', instance.other_property)
+
+    """
+
+    #: Doc comment with some code: property_name = 1
     property_name = 1
-    other_property = 2
+
+    # Regular comment with some code: other_property: int = 2
+    other_property: int = 2
+
+    def some_method(self) -> None:
+        """Comment inside a method."""
+        print('not True and not False')
+
+        # Some logics: count(numbers) or print(False)
+        print(12 + 23 / 3)
