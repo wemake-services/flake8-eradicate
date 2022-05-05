f3db49c6301927584df0b1da235f569482a437d4
wwuck <wirelessduck@gmail.com>
2022-05-05 10:53:32 +1000

    Ignore mypy error on duplicate import
    See https://github.com/python/mypy/issues/1153

--- flake8_eradicate.py
+++ flake8_eradicate.py
@@ -2,9 +2,9 @@ import tokenize
 from typing import Iterable, Iterator, List, Sequence, Tuple, Type
 
 try:
-    from importlib.metadata import version as importlib_version
+    from importlib.metadata import version as ilm_version
 except ImportError:  # pragma: nocover
-    from importlib_metadata import version as importlib_version
+    from importlib_metadata import version as ilm_version  # type: ignore
 
 from eradicate import Eradicator
 from flake8.options.manager import OptionManager
@@ -13,7 +13,7 @@ from flake8.options.manager import OptionManager
 pkg_name = 'flake8-eradicate'
 
 #: We store the version number inside the `pyproject.toml`:
-pkg_version = importlib_version(pkg_name)
+pkg_version = ilm_version(pkg_name)
 
 #: Const for `stdin` mode of `flake8`:
 STDIN = 'stdin'
